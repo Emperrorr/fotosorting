a0fe9ff7e06c483fe628022effd8b309bdd11739
Emperrorr <pshevchenko@mail.ru>
2018-11-27 04:50:45 +0300

    Update sorter.py

--- sorter.py
+++ sorter.py
@@ -11,22 +11,7 @@ def files(path):
             if os.path.isfile(os.path.join(path, file)):
                 yield file
 
-##    print(tags['EXIF DateTimeOriginal'])
-##    t=tags['EXIF DateTimeOriginal']
-#    t=str(t).replace(':','_')[:10]
-###    print(t)
-#    try:
-#        os.mkdir(os.path.join(path, t))
-#    except OSError:
-#        pass
-#    try:
-#        shutil.move(os.path.join(path, file), os.path.join(path, t), copy_function=shutil.copy2)
-#    except shutil.Error:
-#        print (file)
-#        pass
-
 for file in files(path):  
-##    print(file)
     f=open (os.path.join(path, file), 'rb')
     tags=exifread.process_file(f)
     if 'EXIF DateTimeOriginal' in tags:
@@ -35,12 +20,11 @@ for file in files(path):
         t=tags['Image DateTime']
     else:
         t=datetime.datetime.fromtimestamp(os.path.getctime(os.path.join(path, file)))
-##    print(tags['EXIF DateTimeOriginal'])
-##    t=tags['EXIF DateTimeOriginal']
+
     t=str(t).replace(':','_')[:10]
     t=str(t).replace('-','_')[:10]
     f.close()
-##    print(t)
+    
     try:
         os.mkdir(os.path.join(path, t))
     except OSError:
